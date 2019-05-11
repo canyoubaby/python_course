6d42e0cdf44ea26a93346c0a0af2e5d79b780b69
canyoubaby <hieunguyentrung203799@gmail.com>
2019-05-11 21:13:40 +0700

    fix

--- session1/ext5_2.py
+++
@@ -1,4 +0,0 @@
-import time
-loading = '|' *100
-for i in range(100):
-    print(loading[i], sep=' ', end=' ', flush=True); time.sleep(0.1)
--- session1/ext7.py
+++
@@ -1,7 +0,0 @@
-def filter(l, rex):
-    list_l = list()
-    for x in l:
-        if x.startswith(rex):
-            list_l.append(x)
-    print(list_l) 
-filter(['ab', 'bca', 'def' ,'a'], 'a')

71354a185a3fee79a6f1a4d10db054092975a1e0
canyoubaby <hieunguyentrung203799@gmail.com>
2019-05-11 21:18:57 +0700

    ext2.py

---
+++ session1/ext2.py
@@ -0,0 +1,6 @@
+def print_user(name, age, job, email):
+    print(name)
+    print(age)
+    print(job)
+    print(email)
+print_user('Nguyen Trung Hieu',20,'Programming','hieunguyentrung203799@gmail.com')

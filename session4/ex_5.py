0dcfd0e5000c9e839b27b3456881ae4056c2abbe
canyoubaby <hieunguyentrung203799@gmail.com>
2019-05-21 23:35:21 +0700

    session4

--- session4/ex_5.py
+++ session4/ex_5.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-
 import json
 
 
@@ -9,3 +8,25 @@ def get_video_links():
     soup = BeautifulSoup(res.content, 'html.parser')
     list_videos_div = soup.find(name='div', attrs={'class': 'list_video hidden_list width_common'})
     links = list()
+    for vd in list_videos_div.find_all(name='div', attrs={'class': 'each_element item_video'}):
+        data_extend_attr = vd.get('data-extend')
+        link = json.loads(data_extend_attr).get('s360')
+
+        links.append(link)
+
+    return links
+def download_video(link):
+    paths = link.split('/')
+    fn = paths[-1]  # Take the last path segment in URL
+
+    fw = open(fn, mode='wb')
+    res = requests.get(link)
+    fw.write(res.content)
+
+
+links = get_video_links()
+
+for link in links:
+    print(link)
+for link in links[:5]:
+    download_video(link)
